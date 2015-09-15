70fd1ec476f25aca6e555a46e8e0253bb87af897
KCOOK28 <KCOOK28@OLAB-21.ads.ssc.local>
2015-09-15 15:45:40 -0400

    Chapter 4 Assignment

---
+++ CookCh4_10Excersise/CookCh4_10Excersise/GradeBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class GradeBook
+{
+   public string CourseName{get; set; }
+   public string InstructorName { get; set; }
+
+   public GradeBook(string cName, string iName)
+   {
+       CourseName = cName;
+       InstructorName = iName;
+   }
+    public void   DisplayMessage()
+    {
+        Console.WriteLine(" Welcome to Gradebook\n{0}!", CourseName);
+        Console.WriteLine("This course is presented by : \n{0} ", InstructorName);
+    }
+
+  
+}
+
---
+++ CookCh4_10Excersise/CookCh4_10Excersise/GradeBookTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class GradeBookTest
+{
+    public static void Main(String[] args)
+    {
+
+        GradeBook myGradeBook = new GradeBook("CS101 introduction to Programming", "Professor Smith" );
+
+        myGradeBook.DisplayMessage();
+        Console.Read();
+    }
+
+}
