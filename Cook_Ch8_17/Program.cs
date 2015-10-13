16be407a4a129e5cb496d81a8b7fb245a0bcc3c6
KCOOK28 <KCOOK28@live.seminolestate.edu>
2015-10-13 16:19:51 -0400

    Cook Ch 8.17
    Added Comments

--- Cook_Ch8_17/Program.cs
+++ Cook_Ch8_17/Program.cs
@@ -10,27 +10,40 @@ namespace Cook_Ch8_17
     {
         static void Main(string[] args)
         {
+            // assign Diec One
             int dieOne = 0;
+            //asign Dice Two
             int dieTwo = 0;
+            //Assign Sum of both die
             int sum = 0;
+            //create arrary for possible sums
             int[] sumTotals = new int[13];
-            int[,] dieNumbers = new int[7, 7];
+            //create array for each dice number
 
+
+            //create Random object
             Random rand = new Random();
 
-            for(int count = 0;  count < 36000; count++)
+            // loop possible dice rolls
+            for (int count = 0; count < 36000; count++)
             {
+
+                //randomize diceOne
                 dieOne = rand.Next(1, 7);
+                //randomize diceTwo
                 dieTwo = rand.Next(1, 7);
+                //add both die
                 sum = dieOne + dieTwo;
+                //put the sums into total sum array
                 sumTotals[sum] += 1;
-                dieNumbers[dieOne, dieTwo] += 1;
 
             }
 
             Console.WriteLine("The total amount of times that each sum was rolled is listed below:");
             Console.WriteLine();
-            for(int count = 2; count <= 12; count++)
+           
+           // adds the sum number and the amount of times it occurs
+            for (int count = 2; count <= 12; count++)
             {
                 Console.WriteLine("{0}: {1}", count, sumTotals[count]);
             }
