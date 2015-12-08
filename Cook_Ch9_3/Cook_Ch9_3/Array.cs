b737a2169776ffddc54bd1bd413097d2d9103009
KCOOK28 <KCOOK28@live.seminolestate.edu>
2015-12-08 17:41:10 -0500

    Ch9.3 Assignment

---
+++ Cook_Ch9_3/Cook_Ch9_3/Array.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cook_Ch9_3
+{
+    class Array
+    {
+        public int number;
+        public string desc;
+        public int qty;
+        public double rate;
+
+        public Array()
+        {
+ 
+        }
+
+        public Array(int num, string description, int quantity, double price)
+        {
+            number = num;
+            desc = description;
+            qty = quantity;
+            rate = price;
+        }
+
+        public Array[] GetInvoices()
+
+        {
+            Array one = new Array(83, "Electric sender", 7, 57.98);
+            Array two = new Array(24, "Power saw", 18, 99.99);
+            Array three = new Array(7,"Sledge hammer", 11, 21.50);
+            Array four = new Array(77, "Hammer", 76, 11.99);
+            Array five = new Array(39, "Lawn mower" ,3, 79.50);
+            Array six = new Array(68 ,"Screwdriver", 106, 6.99);
+            Array sev = new Array(56, "Jig saw" ,21, 11.00);
+            Array eight = new Array(3, "Wrench" ,34 ,7.50);
+            Array[] invArr = { one, two, three, four, five, six, sev, eight };
+            return invArr;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Cook_Ch9_3/Cook_Ch9_3/InvoiceTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cook_Ch9_3
+{
+    class InvoiceTest
+    {
+        static void Main(string[] args)
+        {
+            Array myInvoice = new Array();
+            Array[] getArr = myInvoice.GetInvoices();
+
+            var sortDesc = from inv in getArr
+                           orderby
+                               inv.desc
+                           select inv;
+
+            Console.WriteLine("Sort by Description: ");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Number\t\tDetail\tQuantity\tPrice");
+            Console.WriteLine("--------------------------------");
+            foreach(var myInv in sortDesc)
+                Console.WriteLine("{0,2}\t{1,15}\t{2,10} \t{3}", myInv.number ,
+                    myInv.desc,myInv.qty,myInv.rate);
+            Console.WriteLine("--------------------------------");
+            
+            var sortPrice = from inv in getArr
+                            orderby inv.rate 
+                            select inv;
+            Console.WriteLine("Sort by Price:");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Number\t\tDetail\tQuanity\tPrice");
+            Console.WriteLine("--------------------------------");
+
+            foreach(var myInv in sortPrice)
+                Console.WriteLine("{0,2}\t{1,15}\t{2,10} \t{3}", myInv.number ,
+                    myInv.desc,myInv.qty,myInv.rate);
+            Console.WriteLine("--------------------------------");
+            
+            var sortQty = from inv
+                          in getArr
+                          orderby
+                             inv.desc
+                             select new{inv.desc, inv.qty};
+
+            Console.WriteLine("Sort by Descriptiob:");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Detail\t Quantity");
+                Console.WriteLine("--------------------------------");
+
+            foreach (var myInv in sortQty)
+                Console.WriteLine("{0, 15}\t{1}", myInv.desc, myInv.qty);
+             Console.WriteLine("--------------------------------");
+              
+             var sortTotal = from inv
+                             in getArr
+                             let  invoiceTotal = inv.qty*inv.rate 
+                             orderby
+                                invoiceTotal select inv;
+
+            Console.WriteLine("Sort by Invoice toal: ");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Number\t\tDetail\tQuantity\tPrice");
+                 Console.WriteLine("--------------------------------");
+
+            foreach(var myInv in sortTotal)
+                Console.WriteLine("{0,2}\t{1,15}\t{2,10}\t{3}", myInv.number, myInv.desc, myInv.qty, myInv.rate);
+            Console.WriteLine("--------------------------------");
+           
+            var sortRange = from inv
+                            in getArr let total = inv.qty*inv.rate 
+                            where total >200 && total<500 
+                            orderby
+                               total ascending 
+                            select inv;
+
+            Console.WriteLine("Sort by Range: ");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Number\t\tDetail\tQuantity\tPrice");
+            Console.WriteLine("--------------------------------");
+
+            foreach (var myInv in sortRange)
+                Console.WriteLine("{0,2}\t{1,15}\t{2,10}\t{3}", myInv.number, myInv.desc, myInv.qty, myInv.rate);
+            Console.WriteLine("--------------------------------");
+            
+            Console.ReadKey();        
+        }
+
+        }
+}
+
